022c28703c317a00725b779f0a410e29cf0776e7
Carsten Paasch <carsten.paasch@obi.de>
2017-01-03 08:07:18 +0100

    Initial commit

---
+++ src/PackageB/PackageB.Test.Unit/ClassOfPackageBTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace PackageB.Test.Unit
+{
+
+    [TestFixture]
+    public class ProgrammTest
+    {
+        [Test]
+        public void GetName_NameIsNotEmpty()
+        {
+            var subject = new ClassOfPackageB();
+            Assert.IsNotNullOrEmpty(subject.GetName());
+        }
+    }
+}
---
+++ src/PackageB/PackageB/ClassOfPackageB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using PackageA;
+
+namespace PackageB
+{
+    public class ClassOfPackageB
+    {
+        public string GetName()
+        {
+            return "PackageB";
+        }
+
+        public string GetVersion()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string productVersion = fileVersionInfo.ProductVersion;
+            return productVersion;
+        }
+
+        public string GetSummary()
+        {            
+            return $"{GetName()} v. {GetVersion()} {Environment.NewLine}{GetInformationAboutUsedPackages()}";
+        }
+
+        private static string GetInformationAboutUsedPackages()
+        {
+            var classOfPackageA = new ClassOfPackageA();
+            var packageInfo = classOfPackageA.GetSummary();
+            return $"Used package: {packageInfo}";
+        }
+    }
+}
